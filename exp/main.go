17af5e71df00c60a0bd3f5a87fb031c92b4702d0
shankar_shivram <shankarramshivram@users.noreply.github.com>
2017-09-29 13:11:30 -0700

    Till Chapter 8.4.4

--- exp/main.go
+++ exp/main.go
@@ -1,22 +1,33 @@
 package main
 
 import (
-	"html/template"
-	"os"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
 )
 
+const (
+	host   = "localhost"
+	port   = 5432
+	user   = "postgres"
+	dbname = "wwgo_dev"
+)
+
+type User struct {
+	gorm.Model
+	Name  string
+	Email string `gorm:"not null;unique_index"`
+}
+
 func main() {
-	t, err := template.ParseFiles("/Users/sshivram/go-workspace/src/wwgo/exp/hello.gohtml")
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable", host, port, user, dbname)
+	db, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
+	defer db.Close()
+	fmt.Println("Success Connection")
+	db.LogMode(true)
+	db.AutoMigrate(&User{})
 
-	data := struct {
-		Name string
-	}{"<script>alert('Howdy!');</script>"}
-
-	err = t.Execute(os.Stdout, data)
-	if err != nil {
-		panic(err)
-	}
 }
