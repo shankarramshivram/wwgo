1168fd1eb547217f5d853d0c20f0484f58402a3d
Shankarram Shivram <shankarram.shivram@am.sony.com>
2017-09-29 15:20:35 -0700

    Till Chapter 8.4.9

--- exp/main.go
+++ exp/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
+	"os"
+	"strings"
 )
 
 const (
@@ -15,9 +18,41 @@ const (
 
 type User struct {
 	gorm.Model
-	Name  string
-	Email string `gorm:"not null;unique_index"`
+	Name   string
+	Email  string `gorm:"not null;unique_index"`
+	Orders []Order
 }
+
+type Order struct {
+	gorm.Model
+	UserID      uint
+	Amount      int
+	Description string
+}
+
+/*
+func getInfo() (name, email string) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("What is your name?")
+	name, _ = reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+	fmt.Println("What is your email?")
+	email, _ = reader.ReadString('\n')
+	email = strings.TrimSpace(email)
+	return name, email
+}
+
+func createOrder(db *gorm.DB, user User, amount int, desc string) {
+	db.Create(&Order{
+		UserID:      user.ID,
+		Amount:      amount,
+		Description: desc,
+	})
+	if db.Error != nil {
+		panic(db.Error)
+	}
+}
+*/
 
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable", host, port, user, dbname)
@@ -28,6 +63,15 @@ func main() {
 	defer db.Close()
 	fmt.Println("Success Connection")
 	db.LogMode(true)
-	db.AutoMigrate(&User{})
+	db.AutoMigrate(&User{}, &Order{})
+
+	var user User
+	db.Preload("Orders").First(&user)
+	if db.Error != nil {
+		panic(db.Error)
+	}
+	fmt.Println("Email:", user.Email)
+	fmt.Println("Number of orders: ", len(user.Orders))
+	fmt.Println("Orders:", user.Orders)
 
 }
